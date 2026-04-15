db5918a9984731143ba24df197906d346a322525
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-04-14 18:17:33 -0700

    Update static array

--- data_structures/arrays/static.py
+++ data_structures/arrays/static.py
@@ -8,61 +8,69 @@ class Array:
         self.size = size
 
     def append(self, val):
-        self.array[self.length] = val
-        self.length += 1
+        if self.length < self.size:
+            self.array[self.length] = val
+            self.length += 1
+        else:
+            print("Error: Array is full.")
+
+
 
     def insert(self, idx, val):
-        if idx <= self.length:
-            temp = self.array[idx]
+        if self.length <self.size and 0 <= idx <= self.length:
+            for i in range(self.length, idx, -1):
+                self.array[i] = self.array[i-1]
             self.array[idx] = val
-            while idx < self.length - 1:
-                next_val = self.array[idx+1]
-                self.array[idx+1] = temp
-                temp = next_val
-                idx += 1
-        self.length += 1
+            self.length += 1
+        else:
+            print("Error: Capacity reached or invalid index.")
+
 
 
     def delete(self, idx):
-        while idx < self.length - 1:
-            next_val = self.array[idx+1]
-            self.array[idx] = next_val
-            idx += 1
-        self.length -= 1
+        if 0 <= idx <= self.length:
+            for i in range(idx, self.length-1):
+                self.array[i] = self.array[i+1]
+            self.array[self.length - 1] = 0
+            self.length -= 1
+        else:
+            print("Error: Invalide index.")
+
 
     def update(self, idx, val):
         if idx <= self.length:
             self.array[idx] = val
 
     def read(self, idx):
-        if idx <= self.length:
+        if idx < self.length:
             return self.array[idx]
 
     def search(self, val):
         for i in range(self.length):
-            print(self.read(i))
             if self.read(i) == val:
-                print(self.array[i])
-                return True
-            return - 1
+                return i
+        return - 1
 
+      
     def __repr__(self):
-        return str(self.array)
+        return str(self.array[:self.length])
     
 
-if "__name__" == "main":
-    array = Array(7)
-    array.append(2)
-    array.append(90)
-    array.append(6)
-    array.append(3)
-    array.append(1)
-    print("=====Array values====")
-    #print(array)
-    array.insert(2, 300)
-    #print("====after inserting 300 at position 2")
-    array.update(3, 1000)
-    array.delete(0)
-    print(array.read(1))
-    print(array.search(3))
-    
+
+array = Array(7)
+array.append(2)
+array.append(90)
+array.append(6)
+array.append(3)
+array.append(1)
+print("=====Array values====")
+print(array)
+array.insert(2, 300)
+print(array)
+array.update(3, 1000)
+print(array)
+array.delete(0)
+print(array)
+print(array.read(1))
+print(array.length)
+print(array.search(100))
