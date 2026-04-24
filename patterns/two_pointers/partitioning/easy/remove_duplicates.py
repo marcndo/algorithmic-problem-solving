345acd0620ea1361b2e41f7760c6ed0c771c941d
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-04-24 10:56:50 -0700

    Removed duplicates

---
+++ patterns/two_pointers/partitioning/easy/remove_duplicates.py
@@ -0,0 +1,13 @@
+"Leetcode 26: Remove duplicates"
+
+def remove_duplicates(nums):
+    slow = 1
+    for i in range(1,len(nums)):
+        if nums[slow - 1] != nums[i] and nums[slow] != nums[i]:
+            nums[slow] = nums[i]
+            slow += 1
+    return slow
+
+
+print(remove_duplicates([1, 1, 1, 2]))
+
