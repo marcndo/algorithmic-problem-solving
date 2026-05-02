9d84204390a13ab4116077dafd9a82f25f68eb59
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-05-02 02:32:33 -0700

    Duplicate zeros

--- patterns/two_pointers/partitioning/easy/duplicate_zeros.py
+++ patterns/two_pointers/partitioning/easy/duplicate_zeros.py
@@ -1,17 +1,17 @@
 "Duplicate zeros"
 
 def duplicate_zeros(nums):
-    n = len(nums) 
-    zeros = nums.count(0)
+    n = len(nums)
     i = n - 1
+    zeros = nums.count(0)
     j = n + zeros - 1
-    while i < j:
+    while i >= 0:
         if j < n:
             nums[j] = nums[i]
         if nums[i] == 0:
             j -= 1
             if j < n:
-                nums[j] = 0
+                nums[j] == 0
         i -= 1
         j -= 1
     return nums
