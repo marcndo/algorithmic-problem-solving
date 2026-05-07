c3611607d11f470618efcd3f1cf392a190b6c4b5
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-05-06 19:24:35 -0700

    Update remove element with new solution

--- patterns/two_pointers/partitioning/easy/remove_elelment.py
+++ patterns/two_pointers/partitioning/easy/remove_elelment.py
@@ -1,5 +1,14 @@
 "Leetcode 27. Remove Element"
 
+def remove_elem(nums, val):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[i] = nums[j]
+            j += 1
+    return j
+
+
 def remove_element(nums, val):
     slow = 0
     for i in range(len(nums)):
@@ -13,9 +22,3 @@ def remove_element(nums, val):
     return slow
 
 
-nums = [3,2,2,3]
-val = 3
-
-nums = [0,1,2,2,3,0,4,2]
-val = 2
-print(remove_element(nums, val))
