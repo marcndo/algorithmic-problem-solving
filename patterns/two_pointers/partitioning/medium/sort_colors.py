f6842b64cb24540746ce555f924c9278b019bda7
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-05-08 19:26:57 -0700

    Sort colors

--- patterns/two_pointers/partitioning/medium/sort_colors.py
+++ patterns/two_pointers/partitioning/medium/sort_colors.py
@@ -1,5 +1,13 @@
 def sort_colors(nums):
-    pass
-
-nums = [2,0,2,1,1,0]
-print(sort_colors(nums))
+    low, mid, high = 0, 0, len(nums) - 1
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 2:
+            nums[high], nums[mid] = nums[mid], nums[high]
+            hight -= 1
+        else:
+            mid += 1
+    return nums
