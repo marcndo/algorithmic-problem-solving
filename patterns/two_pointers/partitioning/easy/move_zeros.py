8d55497fd9316a72c7054fd9e9e930aaf30ee1f9
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-05-09 18:12:27 -0700

    Fix bug in remove move zero problem

--- patterns/two_pointers/partitioning/easy/move_zeros.py
+++ patterns/two_pointers/partitioning/easy/move_zeros.py
@@ -6,6 +6,8 @@ def move_zeros(nums):
         if nums[fast] != 0:
             nums[fast], nums[slow] = nums[slow], nums[fast]
             slow += 1
+    for i in range(j, len(nums)):
+        nums[i] = 0
     return nums
 nums = [0, 1, 0, 3, 12]
 print(move_zeros(nums))
