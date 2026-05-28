295e1e5dc48d262effec6f940ee0cb9d2f0b2b06
Marcel Grossmann <marcelgrossmann@MarcelGrossmann.local>
2026-05-28 21:39:09 +0100

    Update encode decode string

--- data_structures/arrays/medium/encode_decode.py
+++ data_structures/arrays/medium/encode_decode.py
@@ -22,6 +22,38 @@ class EncodeDecode:
             decode += chr(encode[i])
         return decode
 
+#==============method two====================
+def encode(arr):
+    encoded = ""
+    for st in arr:
+        encoded += str(len(st))
+        encoded += "#"
+        encoded += st
+    return encoded
+
+def decode(s):
+    decoded = []
+    i = 0
+    n = len(s)
+    while i < n:
+        j = i
+        while s[j] != "#":
+            j += 1
+        length = int(s[i:j])
+        decoded.append(s[j+1:j+1+length])
+        i = j + 1 + length
+
+    return decoded
+
+if __name__ == "__main__":
+    s = ["hello", "world"]
+    encoded = encode(s)
+    decoded = decode(encoded)
+    result = ""
+    for i in range(len(decoded)):
+        print(decoded[i], end=" ")
+
+
 
 
 
