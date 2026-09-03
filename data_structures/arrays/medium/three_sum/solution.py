f76935f3868faa13218394f62556514bb01c30ba
marcndo <ndowahmarcel@gmail.com>
2026-09-03 09:51:05 +0100

    Refactor three sum solution

--- data_structures/arrays/medium/three_sum/solution.py
+++ data_structures/arrays/medium/three_sum/solution.py
@@ -13,10 +13,10 @@ def threesum(nums):
                 result.append([nums[i], nums[l], nums[r]])
                 l += 1
                 r -= 1
-                while l < r and nums[l] == nums[l-1]:
+                while l < r and nums[l] == nums[l+1]:
                     l += 1
     
-                while l < r and nums[r] == nums[r+1]:
+                while l < r and nums[r] == nums[r-1]:
                     r -= 1
             elif total < 0:
                 l += 1
